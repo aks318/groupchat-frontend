fed511a37990762b432a66a39698c44921a15b7a
aks318 <akashviahwakarma98@gmail.com>
2023-12-03 02:13:33 +0530

    Socket connected

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import { socket } from "./socket";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Client Connected");
+    });
+
+    socket.on("message", (data) => {
+      console.log("data", data);
+    });
+  }, []);
+
+  const handleBtn = async () => {
+    const headers = {
+      "Content-Type": "application/json",
+      "Socket-Id": `${socket.id}`,
+    };
+    console.log(socket.id);
+    await axios.post(
+      "http://localhost:5000/groupchat/user/updateSocket",
+      {},
+      {
+        headers: headers,
+      }
+    );
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleBtn}>Update</button>
     </div>
   );
 }
